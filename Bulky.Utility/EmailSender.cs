6e7c6514ca7f42eb94e4151252ea8b7375fc951e
Thamindu Perera <131575382+Thamindu4420@users.noreply.github.com>
2024-02-06 12:18:37 +0530

    Add files via upload

---
+++ Bulky.Utility/EmailSender.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.AspNetCore.Identity.UI.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Mail;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RaythosAerospace.Utility {
+    public class EmailSender : IEmailSender {
+        public Task SendEmailAsync(string email, string subject, string htmlMessage) {
+
+            //logic to send email
+            //MailMessage message = new MailMessage();
+            //message.From = new MailAddress("kavindu.cyber2@gmail.com", "Raythos Aerospace");
+            //message.To.Add(email);
+            //message.Subject = subject;
+            //message.IsBodyHtml = true;
+            //message.Body ="AAAA";
+            //SmtpClient client = new SmtpClient("smtp.gmail.com");
+            //client.EnableSsl = true;
+            //client.Port = 587;
+            //client.UseDefaultCredentials = true;
+            //client.Credentials = new System.Net.NetworkCredential("kavindu.cyber2@gmail.com", "erkozgxlkqnesoey");
+            //client.Send(message);
+           
+            return Task.CompletedTask;
+        }
+
+        public void SendEmail(string email, string subject, string htmlMessage)
+        {
+
+            MailMessage message = new MailMessage();
+            message.From = new MailAddress("kavindu.cyber2@gmail.com", "Raythos Aerospace");
+            message.To.Add(email);
+            message.Subject = subject;
+            message.IsBodyHtml = true;
+            message.Body = "AAAA";
+            SmtpClient client = new SmtpClient("smtp.gmail.com");
+            client.EnableSsl = true;
+            client.Port = 587;
+            client.UseDefaultCredentials = true;
+            client.Credentials = new System.Net.NetworkCredential("kavindu.cyber2@gmail.com", "lkyfnzgieflvnlsc");
+            client.Send(message);
+        }
+    }
+}
